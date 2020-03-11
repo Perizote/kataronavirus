185e427c8645650df2df03104480342a6ce864dc
Víctor <vbergaes@mercadona.es>
2020-03-11 17:18:59 +0100

    Add failing test for happy test

---
+++ Katacoronavirus/Katacoronavirus/Kata.swift
@@ -0,0 +1,13 @@
+//
+//  Kata.swift
+//  Katacoronavirus
+//
+//  Created by Victor Berga on 11/03/2020.
+//  Copyright © 2020 es.mercadona.tests. All rights reserved.
+//
+
+import Foundation
+
+func output(deaths: Int, lives: Int) -> String {
+    fatalError()
+}
--- Katacoronavirus/KatacoronavirusTests/KatacoronavirusTests.swift
+++ Katacoronavirus/KatacoronavirusTests/KatacoronavirusTests.swift
@@ -10,25 +10,7 @@ import XCTest
 @testable import Katacoronavirus
 
 class KatacoronavirusTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGivenANumberOfDeathsAndLives_formatsTheOutput() {
+        XCTAssertEqual("001:002", output(deaths: 1, lives: 2))
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
