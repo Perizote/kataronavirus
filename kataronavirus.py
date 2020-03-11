0684d41dcc71ea5e1d49b2de9eeadd5e76b646c6
Sergi Chisvert <schisver@mercadona.es>
2020-03-11 17:27:16 +0100

    One person can be killed

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,1 +1,6 @@
-score = '000:000'
+
+class KataronaVirus:
+    score = '000:000'
+
+    def person_killed(self):
+        self.score = '001:000'
--- test_kataronavirus.py
+++ test_kataronavirus.py
@@ -1,6 +1,15 @@
 import kataronavirus
 
+
 class TestKataronaVirus:
 
     def test_initial_score_is_0(self):
-        assert kataronavirus.score == '000:000'
+        scoring = kataronavirus.KataronaVirus()
+
+        assert scoring.score == '000:000'
+
+    def test_score_is_1_when_one_person_died(self):
+        scoring = kataronavirus.KataronaVirus()
+        scoring.person_killed()
+
+        assert scoring.score == '001:000'
