fcd97c0043525a2fd0d59228445b5757b9384c55
Sergi Chisvert <schisver@mercadona.es>
2020-03-11 17:47:54 +0100

    One person can be saved

--- kataronavirus.py
+++ kataronavirus.py
@@ -16,5 +16,8 @@ class KataronaVirus:
     def family_killed(self):
         self.coronavirus_score += self.FAMILY_SCORE
 
+    def person_saved(self):
+        self.humanity_score += self.ONE_PERSON_SCORE
+
     def scoring(self):
         return f'00{self.coronavirus_score}:00{self.humanity_score}'
--- test_kataronavirus.py
+++ test_kataronavirus.py
@@ -8,27 +8,33 @@ class TestKataronaVirus:
 
         assert scoring.scoring() == '000:000'
 
-    def test_score_is_1_when_one_person_died(self):
+    def test_score_is_1_0_when_one_person_died(self):
         scoring = KataronaVirus()
         scoring.person_killed()
 
         assert scoring.scoring() == '001:000'
 
-    def test_score_is_2_when_one_couple_died(self):
+    def test_score_is_2_0_when_one_couple_died(self):
         scoring = KataronaVirus()
         scoring.couple_killed()
 
         assert scoring.scoring() == '002:000'
 
-    def test_score_is_3_when_person_died_and_one_couple_died(self):
+    def test_score_is_3_0_when_person_died_and_one_couple_died(self):
         scoring = KataronaVirus()
         scoring.person_killed()
         scoring.couple_killed()
 
         assert scoring.scoring() == '003:000'
 
-    def test_score_is_4_when_family_dies(self):
+    def test_score_is_4_0_when_family_dies(self):
         scoring = KataronaVirus()
         scoring.family_killed()
 
         assert scoring.scoring() == '004:000'
+
+    def test_score_is_0_1_when_person_saved(self):
+        scoring = KataronaVirus()
+        scoring.person_saved()
+
+        assert scoring.scoring() == '000:001'
