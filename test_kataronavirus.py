baac6c84d4f3d22000eb8cbb89acd7861f6a16be
Sergi Chisvert <schisver@mercadona.es>
2020-03-11 17:36:00 +0100

    Refactor class

--- test_kataronavirus.py
+++ test_kataronavirus.py
@@ -1,27 +1,27 @@
-import kataronavirus
+from kataronavirus.kataronavirus import KataronaVirus
 
 
 class TestKataronaVirus:
 
     def test_initial_score_is_0(self):
-        scoring = kataronavirus.KataronaVirus()
+        scoring = KataronaVirus()
 
         assert scoring.scoring() == '000:000'
 
     def test_score_is_1_when_one_person_died(self):
-        scoring = kataronavirus.KataronaVirus()
+        scoring = KataronaVirus()
         scoring.person_killed()
 
         assert scoring.scoring() == '001:000'
 
     def test_score_is_2_when_one_couple_died(self):
-        scoring = kataronavirus.KataronaVirus()
+        scoring = KataronaVirus()
         scoring.couple_killed()
 
         assert scoring.scoring() == '002:000'
 
     def test_score_is_3_when_person_died_and_one_couple_died(self):
-        scoring = kataronavirus.KataronaVirus()
+        scoring = KataronaVirus()
         scoring.person_killed()
         scoring.couple_killed()
 
