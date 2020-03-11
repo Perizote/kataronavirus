bebdd7870501b664ea01d05977a6b75768ad4ec4
Sergi Chisvert <schisver@mercadona.es>
2020-03-11 17:41:32 +0100

    Remove magic numbers

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,17 +1,21 @@
 
 class KataronaVirus:
+    ONE_PERSON_SCORE = 1
+    COUPLE_SCORE = 2
+    FAMILY_SCORE = 4
+
     coronavirus_score = 0
     humanity_score = 0
     score = '000:000'
 
     def person_killed(self):
-        self.coronavirus_score += 1
+        self.coronavirus_score += self.ONE_PERSON_SCORE
 
     def couple_killed(self):
-        self.coronavirus_score += 2
+        self.coronavirus_score += self.COUPLE_SCORE
 
     def family_killed(self):
-        self.coronavirus_score += 4
+        self.coronavirus_score += self.FAMILY_SCORE
 
     def scoring(self):
         return f'00{self.coronavirus_score}:00{self.humanity_score}'
