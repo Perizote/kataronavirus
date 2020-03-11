fc11854087e2af752c2d885ab0a77c06320f473e
Alex Torres <torres.erades.alex@gmail.com>
2020-03-11 17:14:40 +0100

    First iteration

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should see the score', () => {
+  const { container } = render(<App />);
+
+  expect(container).toHaveTextContent('000:000')
 });
