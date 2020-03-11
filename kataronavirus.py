cf773cf57fb0c5c5ab32f187390a990aac0e1dcf
Daniel Ruiz <druizca@mercadona.es>
2020-03-11 17:19:12 +0100

    Kill one couple

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,7 +1,10 @@
 class CovidScore:
 
     def kill_one(self):
-        ...
+        self._score = '001-000'
 
+    def kill_couple(self):
+        self._score = '002-000'
+        
     def get_score(self) -> str:
-        return '001-000'
+        return self._score
--- test_covidscore.py
+++ test_covidscore.py
@@ -8,3 +8,10 @@ class TestCovidScore:
         score_board.kill_one()
 
         assert score_board.get_score() == "001-000"
+
+    def test_kills_one_couple(self):
+        score_board = CovidScore()
+
+        score_board.kill_couple()
+
+        assert score_board.get_score() == "002-000"
