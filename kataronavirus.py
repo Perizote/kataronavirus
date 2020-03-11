e4da03eb35cfc81a4a147c48d126a478e07809b8
Daniel Ruiz <druizca@mercadona.es>
2020-03-11 17:23:05 +0100

    We can kill up to nine people

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,10 +1,13 @@
 class CovidScore:
 
+    def __init__(self):
+        self._score = 0
+
     def kill_one(self):
-        self._score = '001-000'
+        self._score += 1
 
     def kill_couple(self):
-        self._score = '002-000'
+        self._score +=2
 
     def get_score(self) -> str:
-        return self._score
+        return f'00{self._score}-000'
