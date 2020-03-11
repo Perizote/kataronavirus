bb35a5d4fb719b8889349843d97b68af3f62b6a6
Daniel Ruiz <druizca@mercadona.es>
2020-03-11 17:46:21 +0100

    Refactor magic numbers

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,25 +1,29 @@
 class CovidScore:
 
     SCORE_LENGTH = 3
+    INITIAL_VALUE = 0
+    PERSON_VALUE = 1
+    COUPLE_VALUE = 2
+    FAMILY_VALUE = 4
 
     def __init__(self):
-        self._killed = 0
-        self._healed = 0
+        self._killed = self.INITIAL_VALUE
+        self._healed = self.INITIAL_VALUE
 
     def kill_one(self):
-        self._killed += 1
+        self._killed += self.PERSON_VALUE
 
     def kill_couple(self):
-        self._killed += 2
+        self._killed += self.COUPLE_VALUE
 
     def kill_family(self):
-        self._killed += 4
+        self._killed += self.FAMILY_VALUE
 
     def heal_one(self):
-        self._healed += 1
+        self._healed += self.PERSON_VALUE
 
     def heal_couple(self):
-        self._healed += 2
+        self._healed += self.COUPLE_VALUE
 
     def get_score(self) -> str:
         return f'{self._format(self._killed)}-{self._format(self._healed)}'
