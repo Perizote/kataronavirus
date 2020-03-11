0c9832491d5466f268c44a0c956e41f65e805a69
Sergi Chisvert <schisver@mercadona.es>
2020-03-11 17:33:37 +0100

    One person can be killed

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,9 +1,14 @@
 
 class KataronaVirus:
+    coronavirus_score = 0
+    humanity_score = 0
     score = '000:000'
 
     def person_killed(self):
-        self.score = '001:000'
+        self.coronavirus_score += 1
 
     def couple_killed(self):
-        self.score = '002:000'
+        self.coronavirus_score += 2
+
+    def scoring(self):
+        return f'00{self.coronavirus_score}:00{self.humanity_score}'
--- test_kataronavirus.py
+++ test_kataronavirus.py
@@ -6,16 +6,23 @@ class TestKataronaVirus:
     def test_initial_score_is_0(self):
         scoring = kataronavirus.KataronaVirus()
 
-        assert scoring.score == '000:000'
+        assert scoring.scoring() == '000:000'
 
     def test_score_is_1_when_one_person_died(self):
         scoring = kataronavirus.KataronaVirus()
         scoring.person_killed()
 
-        assert scoring.score == '001:000'
+        assert scoring.scoring() == '001:000'
 
     def test_score_is_2_when_one_couple_died(self):
         scoring = kataronavirus.KataronaVirus()
         scoring.couple_killed()
 
-        assert scoring.score == '002:000'
+        assert scoring.scoring() == '002:000'
+
+    def test_score_is_3_when_person_died_and_one_couple_died(self):
+        scoring = kataronavirus.KataronaVirus()
+        scoring.person_killed()
+        scoring.couple_killed()
+
+        assert scoring.scoring() == '003:000'
