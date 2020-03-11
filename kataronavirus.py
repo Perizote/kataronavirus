97301a28f8e175b8ee2b0167ad3c2aee75766fc7
Daniel Ruiz <druizca@mercadona.es>
2020-03-11 17:36:40 +0100

    Refactor

--- kataronavirus.py
+++ kataronavirus.py
@@ -1,17 +1,22 @@
 class CovidScore:
 
+    SCORE_LENGTH = 3
+
     def __init__(self):
-        self._score = 0
+        self._killed = 0
         self._healed = 0
 
     def kill_one(self):
-        self._score += 1
+        self._killed += 1
 
     def kill_couple(self):
-        self._score +=2
+        self._killed +=2
 
     def heal_one(self):
         self._healed += 1
 
     def get_score(self) -> str:
-        return f'{str(self._score).zfill(3)}-{str(self._healed).zfill(3)}'
+        return f'{self._format(self._killed)}-{self._format(self._healed)}'
+
+    def _format(self, number):
+        return str(number).zfill(self.SCORE_LENGTH)
