66c4ba263045459df36fc804be8333f62d1f6ef0
Víctor <vbergaes@mercadona.es>
2020-03-11 17:10:50 +0100

    Initial commit

---
+++ Katacoronavirus/Katacoronavirus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Katacoronavirus
+//
+//  Created by Victor Berga on 11/03/2020.
+//  Copyright © 2020 es.mercadona.tests. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
