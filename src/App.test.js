aa8c451972625db7db9d3e0f000bc9855b282d8c
Antonio Tarin Gomez <antarin@MacBook-Pro-de-Antonio.local>
2020-05-18 16:06:38 +0200

    [Added] Added killed/healed counter

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Check for counter', () => {
+
+  const { getByLabelText } = render(<App />);
+
+  const counterLabel = getByLabelText('Counter');
+
+  expect(counterLabel).toHaveTextContent('000:000');
+
 });
+// test('', ()=>{
+
+// });
